aded0811680adb0c3ebda2a9acfbfdd023be81e5
vicky leu <yu6564172@gmail.com>
2024-07-09 17:38:56 +0800

    ignore .kotlin folder

--- sample/composeApp/src/commonMain/kotlin/io/wojciechosak/calendar/calendar/App.kt
+++ sample/composeApp/src/commonMain/kotlin/io/wojciechosak/calendar/calendar/App.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -29,7 +29,7 @@ internal fun App() {
             Scaffold(
                 topBar = {
                     val navigator = LocalNavigator.current
-                    SmallTopAppBar(
+					MediumTopAppBar(
                         colors =
                         TopAppBarDefaults.topAppBarColors(
                             containerColor = MaterialTheme.colorScheme.primary,
